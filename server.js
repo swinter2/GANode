f81449ed9554cbf1a83f9631f233b51f82ac0e20
Sam Winter <sam.winter@gmail.com>
2014-04-30 15:06:40 -0400

    Works in IIS now.

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
 var express = require('express'),
     fs = require('fs'),
-    logfmt = require("logfmt");
+    logfmt = require("logfmt"),
+    minify = require('minify');
 var app = express();
+
+var DEBUG = true;
+
+if (!DEBUG) {
+    // Minify all Javascript
+    minify.optimize([
+        './static/scripts/analytics/api.js',
+        './static/scripts/analytics/app.js',
+        './static/scripts/analytics/controllers/Analytics.js'
+    ]);
+}
 
 app.use(logfmt.requestLogger());
 
-app.use("/static", express.static('./static'));
+//app.use("/static", express.static('./static'));
 app.use("/", express.static('./views'));
 
 app.use(function(req, res, next){
+    console.log("Page not found - %s", req.url);
     res.send(404, "Page not found!");
 });
 app.use(function (err, req, res, next) {
@@ -16,8 +29,8 @@ app.use(function (err, req, res, next) {
     res.send(500, "Whoops, there was an error.");
 });
 
-var port = Number(process.env.PORT || 8000);
+var port = process.env.PORT || 8000;
 var server = app.listen(port, function () {
-    console.log("Listening on port %d", server.address().port);
+    console.log("[%s] Listening on port %d", process.pid, server.address().port);
 });
 
